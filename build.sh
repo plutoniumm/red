51ad285c199ec2ab39bf0ac9f1b6c1fa42f5b863
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-09-29 11:08:25 +0530

    just an overall chadded out build

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 echo "\033[0;34mUpdating build...\033[0m"
 rm -rf build
 mkdir build
+mkdir build/assets
 
 echo "\033[0;34mCreating Go Binary\033[0m"
 cd server
 env GOOS=darwin GOARCH=arm64 go build -o ../build/red-mac
 env GOOS=linux GOARCH=arm go build -o ../build/red-nix
-cd ..
+cd ../
 
 echo "\033[0;34mCopying Assets...\033[0m"
 cp -r assets/* build/assets/
@@ -17,3 +18,4 @@ cp demo.sh build/
 rm release.zip
 echo "\033[0;34mGenerating Archive...\033[0m"
 zip -r release.zip build
+rm -rf build
