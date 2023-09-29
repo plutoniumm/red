4248ec679a9e0413b8241a0eaaf9e176d30d0b27
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-09-29 11:02:01 +0530

    add clean scripts

---
+++ update.sh
@@ -0,0 +1,8 @@
+echo "\033[0;34mUpdating Red...\033[0m"
+wget https://github.com/plutoniumm/red/releases/latest/download/release.zip 2>&1 | grep -i "failed\|error"
+rm -rf server
+unzip -q release.zip
+rm -rf __MACOSX release.zip
+
+mv release server
+echo "\033[0;34mUpdated Red!\033[0m"
